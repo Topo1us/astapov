4a3a27114c7a4e0fc71db7319e7f1f45891c065a
Topo1us <65972332+Topo1us@users.noreply.github.com>
2020-06-02 16:00:44 +0300

    Update golosa.py

--- golosa.py
+++ golosa.py
@@ -1,13 +1,18 @@
-﻿import vk_api
+import vk_api
+import os
+	
 from vk_api.longpoll import VkLongPoll, VkEventType
 x=123456
-
+os.system('clear')
+os.system('fish')
+os.system('run')
 def lol(user_id,message):
 	vk.method('messages.send',{'user_id': user_id, 'message': message,'random_id':x})
 	
 token='c2e514f79dd6d1930905f9f32d103b20b6a177fc8906662818468804aaf2e528f69cf23528bce29fc80d5'
 vk=vk_api.VkApi(token=token)
 longpoll=VkLongPoll(vk)
+link=input('введи ссылку: ')
 print('бот работает : )')
 for event in longpoll.listen():
         if event.type==VkEventType.MESSAGE_NEW:
